7ea8b767be8ef332c58f840cebda14a615b7154d
Wietse Wind <mail@wietse.com>
2023-12-04 21:27:54 +0100

    Fix error on no stdDev (all prices equal)

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ export default (async () => {
 
   log(raw)
 
-  const filteredResults = rawResults.filter(r => Math.abs(r - rawMedian) < rawStdev)
+  const filteredResults = rawResults.filter(r => Math.abs(r - rawMedian) < rawStdev || rawStdev === 0)
   const filteredMedian = stats.median(filteredResults)
   const filteredMean = stats.mean(filteredResults)
   
